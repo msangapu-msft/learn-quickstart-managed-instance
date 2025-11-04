8e1c0026a3441bb2aa8cd6526b2aca9c8a26b8a9
Mangesh Sangapu <msangapu@microsoft.com>
2025-11-04 10:15:08 -0600

    Add configuration files for .NET Framework 4.8

---
+++ src/AptosImageDemo/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="AptosImageDemo.WebApiApplication" Language="C#" %>
---
+++ src/AptosImageDemo/Global.asax.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Web;
+using System.Web.Http;
+
+namespace AptosImageDemo
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
